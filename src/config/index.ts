81b15b51b0cf585caf19e85292b94733fb8aa8d8
sanmaru <moni.sanmaru@gmail.com>
2021-01-22 06:05:28 +0900

    실행시 기본 출력 데이터 및 eslint rule 수정

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const env: string = process.env.NODE_ENV || "localhost";
+const env: string = process.env.NODE_ENV || 'localhost';
 
 const configs: any = {
   base: {
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,18 @@ import app from './app';
 
 console.log('KOA Module Start');
 
-if(config.env === 'localhost') console.log( 'http://' + config.host + ':' + config.port );
-//console.info(`Listening to http://localhost:${port});
+switch (config.env) {
+  case 'localhost':
+    console.info(`Listening to http://localhost:${config.port}`);
+    break;
+  case 'development':
+    console.info(`Listening to https://${config.host}:${config.port}`);
+    break;
+  case 'product':
+    console.info(`Listening to https://${config.host}:${config.port}`);
+    break;
+  default:
+    console.info(`Listening to http://127.0.0.1:${config.port}`);
+}
 
 const server = app.listen(config.port);
